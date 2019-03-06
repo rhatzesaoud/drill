Drill4J/drill4j

URLs modified with specification

drill-admin/src/adminMain/kotlin/com/epam/drill/endpoints/PluginDispatcher.kt
@@ -3,15 +3,18 @@ package com.epam.drill.endpoints
 import com.epam.drill.agentmanager.AgentStorage
 import com.epam.drill.plugin.api.end.WsService
 import com.epam.drill.plugins.Plugins
+import com.epam.drill.router.Routes
 import com.epam.drill.storage.MongoClient
 import com.google.gson.Gson
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.cio.websocket.Frame
 import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.Location
-import io.ktor.locations.post
+import io.ktor.locations.patch
 import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.routing
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -61,20 +64,16 @@ class PluginDispatcher(override val kodein: Kodein) : KodeinAware {
         }
     }
 
-
-    @Location("/api/agent/updatePluginConfig/{agentName}/{pluginName}")
-    data class PluginConfig(val agentName: String, val pluginName: String)
-
     init {
         app.routing {
             authenticate {
-                post<PluginConfig> { ll ->
+                patch<Routes.Api.Agent.Agent> { ll ->
                     val message = call.receive<String>()
-                    agentStorage.agents[ll.agentName]?.agentWsSession
+                    agentStorage.agents[ll.agentId]?.agentWsSession
                         ?.send(
                             agentWsMessage("/plugins/updatePluginConfig", message)
                         )
-                    call.respondText { "Update sent" }
+                    call.respond { HttpStatusCode.OK }
                 }
             }
         }

drill-admin/src/adminMain/kotlin/com/epam/drill/endpoints/openapi/SwaggerDrillAdminServer.kt
@@ -8,15 +8,19 @@ import com.epam.drill.endpoints.agentWsMessage
 import com.epam.drill.plugins.Plugins
 import com.epam.drill.plugins.agentPluginPart
 import com.epam.drill.plugins.serverInstance
+import com.epam.drill.router.DevRoutes
 import com.epam.drill.router.Routes
 import com.google.gson.Gson
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.send
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.get
+import io.ktor.locations.patch
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.routing
@@ -42,20 +46,40 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
         app.routing {
             registerAgent()
             registerDrillAdmin()
+            if (app.environment.config.property("ktor.dev").toString().toBoolean()) {
+                registerDrillAdminDev()
+            }
         }
     }
 
     private fun Routing.registerAgent() {
         authenticate {
+            patch<Routes.Api.Agent.Agent> {config ->
+                agentStorage.agents[config.agentId]?.agentWsSession?.send(
+                    Frame.Text(
+                        Gson().toJson(
+                            Message(
+                                MessageType.MESSAGE,
+                                "/agent/updateAgentConfig",
+                                call.receive()
+                            )
+                        )
+                    )
+                )
+                call.respond { HttpStatusCode.OK }
+            }
+        }
+
+        authenticate {
             get<Routes.Api.Agent.UnloadPlugin> { up ->
-                val drillAgent: DrillAgent? = agentStorage.agents[up.agentName]
+                val drillAgent: DrillAgent? = agentStorage.agents[up.agentId]
                 if (drillAgent == null) {
-                    call.respond("can't find the agent '${up.agentName}'")
+                    call.respond("can't find the agent '${up.agentId}'")
                     return@get
                 }
                 val agentPluginPartFile = plugins.plugins[up.pluginName]?.agentPluginPart
                 if (agentPluginPartFile == null) {
-                    call.respond("can't find the plugin '${up.pluginName}' in the agent '${up.agentName}'")
+                    call.respond("can't find the plugin '${up.pluginName}' in the agent '${up.agentId}'")
                     return@get
                 }
 
@@ -67,14 +91,14 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
 
         authenticate {
             get<Routes.Api.Agent.LoadPlugin> { lp ->
-                val drillAgent: DrillAgent? = agentStorage.agents[lp.agentName]
+                val drillAgent: DrillAgent? = agentStorage.agents[lp.agentId]
                 if (drillAgent == null) {
-                    call.respond("can't find the agent '${lp.agentName}'")
+                    call.respond("can't find the agent '${lp.agentId}'")
                     return@get
                 }
                 val agentPluginPartFile = plugins.plugins[lp.pluginName]?.agentPluginPart
                 if (agentPluginPartFile == null) {
-                    call.respond("can't find the plugin '${lp.pluginName}' in the agent '${lp.agentName}'")
+                    call.respond("can't find the plugin '${lp.pluginName}' in the agent '${lp.agentId}'")
                     return@get
                 }
                 val inChannel: FileChannel = agentPluginPartFile.inputStream().channel
@@ -105,18 +129,13 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
 //                println(put.array().contentToString())
 //                put.flip()
                 drillAgent.agentWsSession.send(Frame.Binary(true, put))
-                call.respond("event 'load' and plugin's file(${lp.agentName}) was sent to AGENT")
+                call.respond("event 'load' and plugin's file(${lp.agentId}) was sent to AGENT")
             }
         }
 
         authenticate {
-            get<Routes.Api.Agent.Agents> {
-                call.respond(agentStorage.agents.values.map { da -> da.agentInfo })
-            }
-        }
-        authenticate {
             get<Routes.Api.Agent.Agent> { up ->
-                call.respond(agentStorage.agents[up.agentName]?.agentInfo!!)
+                call.respond(agentStorage.agents[up.agentId]?.agentInfo!!)
             }
         }
     }
@@ -128,6 +147,17 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
         authenticate {
             get<Routes.Api.AllPlugins> {
                 call.respond(plugins.plugins.values.map { dp -> dp.serverInstance.id })
+            }
+        }
+    }
+
+    /**
+     * drill-admin-dev
+     */
+    private fun Routing.registerDrillAdminDev() {
+        authenticate {
+            get<DevRoutes.Api.Agent.Agents> {
+                call.respond(agentStorage.agents.values.map { da -> da.agentInfo })
             }
         }
     }

drill-admin/src/adminMain/kotlin/com/epam/drill/router/Routers.kt
@@ -10,31 +10,31 @@ object Routes {
     class Api {
         @Location("/agent")
         class Agent {
-                @Location("/loadPlugin/{agentName}/{pluginName}")
-                data class LoadPlugin(override val agentName: String, override val pluginName: String) : PluginManageLink()
+            @Location("/{agentId}/toggle-standby")
+            class AgentToggleStandby
 
-                @Location("/unloadPlugin/{agentName}/{pluginName}")
-                data class UnloadPlugin(override val agentName: String, override val pluginName: String) : PluginManageLink()
+            @Location("/load-plugin/{agentId}/{pluginName}")
+            data class LoadPlugin(override val agentId: String, override val pluginName: String) : PluginManageLink()
 
-                @Location("/agent-info/getAllAgents")
-                class Agents
+            @Location("/unload-plugin/{agentId}/{pluginName}")
+            data class UnloadPlugin(override val agentId: String, override val pluginName: String) : PluginManageLink()
 
-                @Location("/agent-info/{agentName}")
-                data class Agent(val agentName: String)
+            @Location("{agentId}")
+            data class Agent(val agentId: String)
 
-                abstract class PluginManageLink {
-                        abstract val agentName: String
-                        abstract val pluginName: String
-                }
+            abstract class PluginManageLink {
+                abstract val agentId: String
+                abstract val pluginName: String
+            }
         }
 
-        @Location("/pluginContent/{pluginId}")
+        @Location("/plugin-content/{pluginId}")
         data class PluginContent(val pluginId: String)
 
-        @Location("/drill-admin/plugin/getAllPlugins")
+        @Location("/drill-admin/plugin/get-all-plugins")
         class AllPlugins
 
-        @Location("/plugin-info/getPluginsConfiguration")
+        @Location("/plugin-info/get-plugins-configuration")
         class PluginConfiguration
 
         @Location("/login")

rhatzesaoud/drill

src/adminMain/kotlin/com/epam/drill/endpoints/PluginDispatcher.kt
@@ -3,15 +3,18 @@ package com.epam.drill.endpoints
 import com.epam.drill.agentmanager.AgentStorage
 import com.epam.drill.plugin.api.end.WsService
 import com.epam.drill.plugins.Plugins
+import com.epam.drill.router.Routes
 import com.epam.drill.storage.MongoClient
 import com.google.gson.Gson
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.cio.websocket.Frame
 import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.Location
-import io.ktor.locations.post
+import io.ktor.locations.patch
 import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.routing
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -61,20 +64,26 @@ class PluginDispatcher(override val kodein: Kodein) : KodeinAware {
         }
     }
 
-
-    @Location("/api/agent/updatePluginConfig/{agentName}/{pluginName}")
-    data class PluginConfig(val agentName: String, val pluginName: String)
-
     init {
         app.routing {
             authenticate {
-                post<PluginConfig> { ll ->
+                patch<Routes.Api.Agent.UpdatePlugin> { ll ->
                     val message = call.receive<String>()
                     agentStorage.agents[ll.agentName]?.agentWsSession
                         ?.send(
                             agentWsMessage("/plugins/updatePluginConfig", message)
                         )
-                    call.respondText { "Update sent" }
+                    call.respond { HttpStatusCode.OK }
+                }
+            }
+
+            authenticate {
+                patch<Routes.Api.Agent.TogglePlugin> { ll ->
+                    agentStorage.agents[ll.agentName]?.agentWsSession
+                        ?.send(
+                            agentWsMessage("/plugins/togglePlugin", ll.pluginId)
+                        )
+                    call.respond { HttpStatusCode.OK }
                 }
             }
         }

src/adminMain/kotlin/com/epam/drill/endpoints/openapi/SwaggerDrillAdminServer.kt
@@ -8,15 +8,18 @@ import com.epam.drill.endpoints.agentWsMessage
 import com.epam.drill.plugins.Plugins
 import com.epam.drill.plugins.agentPluginPart
 import com.epam.drill.plugins.serverInstance
+import com.epam.drill.router.DevRoutes
 import com.epam.drill.router.Routes
 import com.google.gson.Gson
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.cio.websocket.Frame
-import io.ktor.http.cio.websocket.send
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.get
+import io.ktor.locations.patch
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.routing
@@ -42,10 +45,30 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
         app.routing {
             registerAgent()
             registerDrillAdmin()
+            if (app.environment.config.property("ktor.dev").getString().toBoolean()) {
+                registerDevDrillAdmin()
+            }
         }
     }
 
     private fun Routing.registerAgent() {
+        authenticate {
+            patch<Routes.Api.Agent.Agent> {config ->
+                agentStorage.agents[config.agentName]?.agentWsSession?.send(
+                    Frame.Text(
+                        Gson().toJson(
+                            Message(
+                                MessageType.MESSAGE,
+                                "/agent/updateAgentConfig",
+                                call.receive()
+                            )
+                        )
+                    )
+                )
+                call.respond { HttpStatusCode.OK }
+            }
+        }
+
         authenticate {
             get<Routes.Api.Agent.UnloadPlugin> { up ->
                 val drillAgent: DrillAgent? = agentStorage.agents[up.agentName]
@@ -110,13 +133,19 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
         }
 
         authenticate {
-            get<Routes.Api.Agent.Agents> {
-                call.respond(agentStorage.agents.values.map { da -> da.agentInfo })
-            }
-        }
-        authenticate {
             get<Routes.Api.Agent.Agent> { up ->
                 call.respond(agentStorage.agents[up.agentName]?.agentInfo!!)
+            }
+        }
+
+        authenticate {
+            get<Routes.Api.Agent.AgentToggleStandby> {
+                val agentId = call.receive<String>()
+                agentStorage.agents[agentId]?.agentWsSession
+                    ?.send(
+                        agentWsMessage("agent/toggleStandBy", agentId)
+                    )
+                call.respond { HttpStatusCode.OK }
             }
         }
     }
@@ -128,6 +157,17 @@ class SwaggerDrillAdminServer(override val kodein: Kodein) : KodeinAware {
         authenticate {
             get<Routes.Api.AllPlugins> {
                 call.respond(plugins.plugins.values.map { dp -> dp.serverInstance.id })
+            }
+        }
+    }
+
+    /**
+     * drill-admin-dev
+     */
+    private fun Routing.registerDevDrillAdmin() {
+        authenticate {
+            get<DevRoutes.Api.Agent.Agents> {
+                call.respond(agentStorage.agents.values.map { da -> da.agentInfo })
             }
         }
     }

src/adminMain/kotlin/com/epam/drill/router/Routers.kt
@@ -10,31 +10,37 @@ object Routes {
     class Api {
         @Location("/agent")
         class Agent {
-                @Location("/loadPlugin/{agentName}/{pluginName}")
-                data class LoadPlugin(override val agentName: String, override val pluginName: String) : PluginManageLink()
+            @Location("/{agentName}/toggle-standby")
+            class AgentToggleStandby
 
-                @Location("/unloadPlugin/{agentName}/{pluginName}")
-                data class UnloadPlugin(override val agentName: String, override val pluginName: String) : PluginManageLink()
+            @Location("/load-plugin/{agentName}/{pluginName}")
+            data class LoadPlugin(override val agentName: String, override val pluginName: String) : PluginManageLink()
 
-                @Location("/agent-info/getAllAgents")
-                class Agents
+            @Location("/unload-plugin/{agentName}/{pluginName}")
+            data class UnloadPlugin(override val agentName: String, override val pluginName: String) : PluginManageLink()
 
-                @Location("/agent-info/{agentName}")
-                data class Agent(val agentName: String)
+            @Location("/{agentName}")
+            data class Agent(val agentName: String)
 
-                abstract class PluginManageLink {
-                        abstract val agentName: String
-                        abstract val pluginName: String
-                }
+            @Location("/{agentName}/update-plugin")
+            data class UpdatePlugin(val agentName: String)
+
+            @Location("/{agentName}/{pluginId}/toggle-plugin")
+            data class TogglePlugin(val agentName: String, val pluginId: String)
+
+            abstract class PluginManageLink {
+                abstract val agentName: String
+                abstract val pluginName: String
+            }
         }
 
-        @Location("/pluginContent/{pluginId}")
+        @Location("/plugin-content/{pluginId}")
         data class PluginContent(val pluginId: String)
 
-        @Location("/drill-admin/plugin/getAllPlugins")
+        @Location("/drill-admin/plugin/get-all-plugins")
         class AllPlugins
 
-        @Location("/plugin-info/getPluginsConfiguration")
+        @Location("/plugin-info/get-plugins-configuration")
         class PluginConfiguration
 
         @Location("/login")