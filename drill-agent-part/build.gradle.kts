b999b4cb5cf0bcd94d81e4bf3aa0cc9bf5a53bb7
IgorKey <Igor_Kuzminykh@epam.com>
2019-09-18 10:17:19 +0300

    fixed path of external publish script

--- drill-agent-part/build.gradle.kts
+++ drill-agent-part/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.jfrog.artifactory") version ("4.9.8")
     id("com.jfrog.bintray") version ("1.8.3")
 }
-apply(from = "https://gist.githubusercontent.com/IgorKey/e7a0e07428b6e56283d08dbc605bb942/raw/0af997f4044c4d1e1667e9ee67b2ebdf736fabde/publish.gradle")
+apply(from = "https://raw.githubusercontent.com/Drill4J/build-scripts/master/publish.gradle")
 
 kotlin {
     targets {
