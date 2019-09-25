fb8a0f8de60fc0c46cbb60974f8431f2a67f36a9
IgorKey <Igor_Kuzminykh@epam.com>
2019-09-25 14:22:44 +0300

    fixed compilation problem. moved version calculation to native git library

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -2,5 +2,4 @@ val serializationRuntimeVersion = "0.12.0"
 
 val ktorVersion = "1.2.4"
 
-val drillCommonLibVersion = "0.1.1"
-val drillPluginApiVersion = "0.3.0"
+val drillCommonLibVersion = "0.1.2"
