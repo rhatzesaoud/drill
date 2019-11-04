7a9414cc161b9935c2f0b4c6389aa9645384e38e
IgorKey <Igor_Kuzminykh@epam.com>
2019-11-04 18:52:36 +0300

    try to use build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,11 @@ include(":common")
 include(":plugin-api")
 include(":plugin-api:drill-admin-part")
 include(":plugin-api:drill-agent-part")
+buildCache {
+    local<DirectoryBuildCache> {
+        directory = File(rootDir, "build-cache")
+        removeUnusedEntriesAfterDays = 30
+    }
+}
 
 enableFeaturePreview("GRADLE_METADATA")
