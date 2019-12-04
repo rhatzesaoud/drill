b545a64d3ce42c5aa9b06b562b47467919908891
Kristina Smirnova <kristina_smirnova@epam.com>
2019-12-04 10:12:27 +0300

    EPMDJ-1478. Removed agent path duplication from agent launch command

--- agent/util/src/macosX64Main/kotlin/com/epam/drill/Files.kt
+++ agent/util/src/macosX64Main/kotlin/com/epam/drill/Files.kt
@@ -3,9 +3,14 @@ package com.epam.drill
 import platform.posix.*
 import kotlinx.cinterop.*
 
+val tempPath = "/tmp"
 
-fun doMkdir(path:String){
+val processInfoCmd = "ps -p ${getpid()} -o args"
+
+fun AutofreeScope.openPipe(): CPointer<FILE>? = popen(processInfoCmd, "r")
+
+fun CPointer<FILE>?.close() = pclose(this)
+
+fun doMkdir(path: String) {
     mkdir(path, "0777".toInt(8).convert())
 }
-
-val tempPath = "/tmp"
--- agent/util/src/mingwX64Main/kotlin/com/epam/drill/Files.kt
+++ agent/util/src/mingwX64Main/kotlin/com/epam/drill/Files.kt
@@ -1,9 +1,20 @@
 package com.epam.drill
 
+import kotlinx.cinterop.*
 import platform.posix.*
 
-fun doMkdir(path:String){
-    mkdir(path)
+val tempPath = ""
+
+val processInfoCmd = "wmic process where \"processid='${getpid()}'\" get commandline"
+
+fun AutofreeScope.openPipe(): CPointer<FILE>? {
+    val cmdByteVar = processInfoCmd.cstr.getPointer(this)
+    val mode = "r".cstr.getPointer(this)
+    return popen?.invoke(cmdByteVar, mode)
 }
 
-val tempPath = ""
+fun CPointer<FILE>?.close() = pclose?.invoke(this)
+
+fun doMkdir(path: String) {
+    mkdir(path)
+}
