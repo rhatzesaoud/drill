ad9b59ba2db9261a13e0e0c41a7c46dacbc8ac3b
Viktor Bellavin <viktor_bellavin@epam.com>
2020-01-29 19:05:37 +0300

    Minor improvements: configuration avoidance, base package in admin plugin api, removed unused class

--- plugin-api/drill-admin-part/src/commonMain/kotlin/AgentInfo.kt
+++ plugin-api/drill-admin-part/src/commonMain/kotlin/AgentInfo.kt
@@ -48,4 +48,3 @@ enum class AgentStatus {
     OFFLINE,
     BUSY;
 }
-
--- plugin-api/drill-admin-part/src/jvmMain/kotlin/Sender.kt
+++ plugin-api/drill-admin-part/src/jvmMain/kotlin/Sender.kt
@@ -1,7 +1,5 @@
 package com.epam.drill.plugin.api.end
 
-import com.epam.drill.common.*
-
 interface Sender {
     suspend fun send(agentId: String, buildVersion: String, destination: Any, message: Any)
 }
