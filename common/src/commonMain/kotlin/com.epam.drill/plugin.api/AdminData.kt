45d4049363c7b00151a8e866228aad784cdcb406
Kristina Smirnova <kristina_smirnova@epam.com>
2020-03-12 15:25:09 +0300

    EPMDJ-2468 Removed redundant field from BuildManager

--- common/src/commonMain/kotlin/com.epam.drill/plugin.api/AdminData.kt
+++ common/src/commonMain/kotlin/com.epam.drill/plugin.api/AdminData.kt
@@ -7,9 +7,6 @@ interface AdminData {
 }
 
 interface BuildManager {
-    @Deprecated(message = "Exposure of inner state", replaceWith = ReplaceWith("builds"))
-    val buildInfos: Map<String, BuildInfo>
-
     val builds: Collection<BuildInfo>
 
     operator fun get(version: String): BuildInfo?
